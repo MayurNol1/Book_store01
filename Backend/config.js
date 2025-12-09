b3a7bb156f7592179a4e0ee05aeb1dc66af77e6c
Mayur Nol <mayurnol111@gmail.com>
2025-12-09 23:25:25 +0530

    complete backend

--- Backend/config.js
+++ Backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  'mongodb+srv://mayurnol111:Mayur@4212@cluster0.54g2x.mongodb.net/?appName=Cluster0';
+  'mongodb+srv://mayurnol111:Mayur4212@cluster0.54g2x.mongodb.net/?appName=Cluster0';
